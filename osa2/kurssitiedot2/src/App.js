d0debd576e818550fc82a625737d89af9c2a58c9
Mikko Johansson <mikko.johansson2@gmail.com>
2022-06-06 12:19:51 +0300

    Initial state

--- osa2/kurssitiedot2/src/App.js
+++ osa2/kurssitiedot2/src/App.js
@@ -31,30 +31,36 @@ const Total = (props) => {
   )
 }
 
+const Course = (props) => {
+
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   )
 }
