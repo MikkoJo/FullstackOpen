29aa62770f6c4a9acd46dcc52c6ceab37dff614b
Mikko Johansson <mikko.johansson2@gmail.com>
2022-06-06 13:25:25 +0300

    Steps 6-8 done

--- osa2/kurssitiedot2/src/App.js
+++ osa2/kurssitiedot2/src/App.js
@@ -1,38 +1,39 @@
-const Part = (props) => {
+const Part = ({ part }) => {
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
 
-const Header = (props) => {
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
+const Header = ({ course }) => <h1>{course.name}</h1>
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {parts.map(part =>
+        <Part part={part} key={part.id} />
+      )}
     </div>
   )
 }
 
-const Total = (props) => {
+const Total = ({ parts }) => {
   return (
-    <p>Number of exercises {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
+    <p>Number of exercises: {parts.reduce((total, part) => total + part.exercises, 0)}
     </p>
   )
 }
 
-const Course = (props) => {
-
+const Course = ({ course }) => {
+  // console.log(course)
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
 }
 
 const App = () => {
@@ -54,6 +55,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
